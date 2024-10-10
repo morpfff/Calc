f5c4b56117fcfa3248a9d8dcfb8e1fdbe8b83f9e
morpfff <aronnydayz@gmail.com>
2024-10-10 16:08:39 +0300

    Сложение и вычитание двух чисел

--- Program.cs
+++ Program.cs
@@ -10,13 +10,15 @@ namespace ipiska
     {
         static void Main(string[] args)
         {
-            int a, b, resultadd;
+            int a, b, resultadd, resultsub;
             Console.WriteLine("vvedite pervoe pislo");
             a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("vvedite vtoroe 4islo");
             b = Convert.ToInt32(Console.ReadLine());
             resultadd = a + b;
             Console.WriteLine("Summa = "+resultadd);
+            resultsub = a - b;
+            Console.WriteLine("Raznost' = " + resultsub);
             Console.ReadKey();
         }
     }
